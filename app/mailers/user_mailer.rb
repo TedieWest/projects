5ad79e77b0c3030d3927bfc4519bee584b97904e
Tedie West <tedie.west@gmail.com>
2016-03-19 18:50:25 -0500

    test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,17 @@
-class UserMailer < ApplicationMailer
+class UserMailer < Devise::Mailer
 	  default from: "tedie.west@gmail.com"  #NOTE: when using gmail to send; like we are doing this gets overridden
 
 	  def welcome_email(user)
 	  	@user = user
 	  	mail(to: @user.email, subject: 'Welcome to Projects')
+	  end
+
+	  def reset_password_instructions(record)
+	  end
+
+	  def confirmation_instructions(record)
+	  end
+	  
+	  def unlock_instructions(record)
 	  end
 end
